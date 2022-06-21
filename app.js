22e22a6e578fdb63094305cdbf08fc9a30e232ba
yogis75 <yogi.s75@gmail.com>
2022-06-21 17:17:13 +0530

    Basic Javascript

---
+++ app.js
@@ -0,0 +1,32 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+//Functions
+function addTodo(event) {
+  event.preventDefault();
+  //Todo DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //Create Listeners
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "hey";
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  //Check mark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = "<i class='fas fa-check'></i>";
+  completedButton.classList.add("completed-btn");
+  todoDiv.appendChild(completedButton);
+  //Trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = "<i class='fas fa-trash'></i>";
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  //Append to Todo list
+  todoList.appendChild(todoDiv);
+}
